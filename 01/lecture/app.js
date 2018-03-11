ff50c659ff844fe5c0f9b63133bf927bfc910c74
haeguri <haegyun821@gmail.com>
2018-03-11 18:14:29 +0900

    폴더 구조 수정

--- 01/lecture/app.js
+++ 01/lecture/app.js
@@ -51,40 +51,7 @@ const Table = (_=>{
                     );
                     return table;
                 }, table)
-            )
-
-            // // mycode... 
-            // const {title, header, items} = this[Private];
-            // if(typeof title !== 'string' || !title) throw 'invalid title';
-            // if(!header.length) throw 'no header';
-
-            // const table = document.createElement('table');
-
-            // const caption = table.appendChild(document.createElement('caption'));
-            // caption.innerHTML = title;
-
-            // const thead = table.appendChild(document.createElement('thead'));
-            // const theadTr = thead.appendChild(document.createElement('tr'));
-            // header.reduce((tr, h) => {
-            //     const td = document.createElement('td');
-            //     td.innerHTML = h;
-            //     tr.appendChild(td);
-            //     return tr;
-            // }, theadTr);
-
-            // const tbody = table.appendChild(document.createElement('tbody'));
-            // items.reduce((tbody, item) => {
-            //     const tr = tbody.appendChild(document.createElement('tr'));
-            //     item.reduce((tr, v) => {
-            //         const td = document.createElement('td');
-            //         td.innerHTML = v;
-            //         tr.appendChild(td);
-            //         return tr;
-            //     }, tr);
-            //     return tbody;
-            // }, tbody);
-
-            // document.querySelector('#data').appendChild(table);
+            );
         }
     }
 })();
